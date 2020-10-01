b7ff6e9ed8e826487ceea38b5457a88f73951f8a
aydig8139 <aydig8139@wrdsb.ca>
2020-10-01 15:35:21 +0000

    finished question to spell out a given integer

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * writes out the number given
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,48 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // import scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask user for a number
+    System.out.println("Please enter a positive integer:");
+    int usersNumber = input.nextInt();
+
+    //determine if line is in range and spell out the number
+    switch(usersNumber){
+      case 1:
+       System.out.println("one");
+       break;
+      case 2:
+       System.out.println("two");
+       break;
+      case 3:
+       System.out.println("three");
+       break;
+      case 4:
+       System.out.println("four");
+       break;
+      case 5:
+       System.out.println("five");
+       break;
+      case 6:
+       System.out.println("six");
+       break;
+      case 7:
+       System.out.println("seven");
+       break;
+      case 8:
+       System.out.println("eight");
+       break;
+      case 9:
+       System.out.println("nine");
+       break;
+      case 10:
+       System.out.println("ten");
+       break;
+      default:
+      System.out.println("not in the range");
+    }
     
   }
 }
